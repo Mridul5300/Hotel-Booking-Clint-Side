1d38c49dbb96d39e659bd9baa31b408dd24809ad
Mridul5300 <mridulsarkar1157@gmail.com>
2024-05-09 23:13:30 +0600

    fig file new setup

--- src/FIreBase/firebase config.js
+++ src/FIreBase/firebase config.js
@@ -15,9 +15,9 @@ const firebaseConfig = {
 apiKey: import.meta.env.VITE_apiKey,
 authDomain: import.meta.env.VITE_authDomain,
 projectId: import.meta.env.VITE_projectId,
-storageBucket: import.meta.env.TE_storageBucket,
+storageBucket: import.meta.env.VITE_storageBucket,
 messagingSenderId: import.meta.env.VITEessagingSenderId,
-appId: import.meta.env.VITE_appId
+appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
